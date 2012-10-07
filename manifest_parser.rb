b39530a385e3ac65c2cb4c2d0ca89e2c8eef2d3b
ShaneQful <shane_quigley@hotmail.com>
2012-10-07 17:44:38 +0100

    Got parser working with Decrypter Object for Blowfish Encrypted files

--- manifest_parser.rb
+++ manifest_parser.rb
@@ -1,4 +1,5 @@
 require 'rexml/document'
+require 'base64'
 
 class ManifestParser
 	def initialize file_path
@@ -7,46 +8,77 @@ class ManifestParser
 		@decrypter_hash = Hash.new
 	end
 	
-# 	attr_accessor:encrypted_text, :hashing_algorithm, :size, :salt, :iteration_count, :initialization_vector,:checksum, :decryption_algorithm
 	def parse
 		content_element = find_context_xml()
-		@decrypter_hash['size'] = content_element.attributes['size']
+		@decrypter_hash['size'] = content_element.attributes['size'].to_i
 		content_element.elements.each do |encryption_data|
-			@decrypter_hash['checksum'] = encryption_data.attributes['checksum']
+			@decrypter_hash['checksum'] = Base64.decode64(encryption_data.attributes['checksum'])
 			@decrypter_hash['checksum-type'] = encryption_data.attributes['checksum-type']
 			encryption_data.elements.each do |element|
 				put_attributes_in_the_map element
 			end
 		end
+		#Turn them all into the right format for ruby to understand
+		set_ruby_name_for_encryption
+		set_ruby_name_for_hashing_algorithm
+		if(@decrypter_hash['key-size'] == nil)#Saw a encrypted blowfish odt online with out key size
+			@decrypter_hash['key-size'] = 16
+		end
 		return @decrypter_hash
 	end
 	
+	attr_reader :decrypter_hash
+	private	
+	def set_ruby_name_for_hashing_algorithm
+		if(@decrypter_hash['hashing_algorithm'].include?('SHA1') || @decrypter_hash['hashing_algorithm'].include?('sha1'))
+			@decrypter_hash['hashing_algorithm'] = 'SHA1'
+		elsif(@decrypter_hash['hashing_algorithm'].include?('SHA256') || @decrypter_hash['hashing_algorithm'].include?('sha256'))
+			@decrypter_hash['hashing_algorithm'] = 'SHA256'
+		end
+	end
+	
+	def set_ruby_name_for_encryption
+		decryption_algorithm = ''
+		if(@decrypter_hash['decryption_algorithm'].include?('Blowfish'))
+			decryption_algorithm = 'BF'
+		elsif(@decrypter_hash['decryption_algorithm'].include?('aes256'))
+			decryption_algorithm = 'AES-256'
+		end
+		if(@decrypter_hash['decryption_algorithm'].include?('CBC') ||@decrypter_hash['decryption_algorithm'].include?('cbc'))
+			decryption_algorithm += '-CBC'
+		elsif(@decrypter_hash['decryption_algorithm'].include?('CFB') ||@decrypter_hash['decryption_algorithm'].include?('cfb'))
+			decryption_algorithm += '-CFB'
+		end
+		@decrypter_hash['decryption_algorithm'] = decryption_algorithm
+	end
 	#Thank god for code folding hiding the shame of this method
 	def put_attributes_in_the_map element
 		if(element.attributes['algorithm-name'])
 			@decrypter_hash['decryption_algorithm'] = element.attributes['algorithm-name']
 		end
 		if(element.attributes['initialisation-vector'])
-			@decrypter_hash['initialization_vector'] = element.attributes['initialisation-vector']
+			@decrypter_hash['initialization_vector'] = Base64.decode64(element.attributes['initialisation-vector'])
 		end
 		if(element.attributes['key-derivation-name'])
 			@decrypter_hash['key-derivation-name'] = element.attributes['key-derivation-name']
 		end
 		if(element.attributes['iteration-count'])
-			@decrypter_hash['iteration_count'] = element.attributes['iteration-count']
+			@decrypter_hash['iteration_count'] = element.attributes['iteration-count'].to_i
 		end
 		if(element.attributes['salt'])
-			@decrypter_hash['salt'] = element.attributes['salt']
+			@decrypter_hash['salt'] = Base64.decode64(element.attributes['salt'])
 		end
 		if(element.attributes['start-key-generation-name'])
 			@decrypter_hash['hashing_algorithm'] = element.attributes['start-key-generation-name']
 		end
-		if(element.attributes['key-size'])
-			@decrypter_hash['key-size'] = element.attributes['key-size']
+		if(element.attributes['key-size'] && !@decrypter_hash['key-size'])#I only want the keysize for the derivation algorithm
+			@decrypter_hash['key-size'] = element.attributes['key-size'].to_i
 		end
 	end
 	
 	#Proabably should do this recursively
+	#Might find the smallest file rather than content.xml
+	#because I believe that would help peformance
 	def find_context_xml
 		@manifest.elements.each do |e|
 			e.elements.each do |e1|
@@ -58,9 +90,10 @@ class ManifestParser
 		#Throw exception if it gets this far
 		"Something has gone terribly wrong :("
 	end
-	
-	attr_reader :decrypter_hash
-	private :find_context_xml, :put_attributes_in_the_map
 end
 
-# parser = ManifestParser
+# if __FILE__ == $0
+# 	parser = ManifestParser.new ARGV[0]
+# 	parser.parse
+# 	puts parser.decrypter_hash.inspect
+# end
--- odf_decrpter.rb
+++ odf_decrpter.rb
@@ -3,31 +3,18 @@
 require 'openssl'
 require 'base64'
 require 'zlib'
+require 'manifest_parser.rb'
 
 class ODFDecrpter
-	def initialize #content_file
-# 		file = File.open(content_file,'rb') #Content xml from odf
-# 		@encrypted_text = ''
-# 		file.readlines.each do |line|
-# 			@encrypted_text += line
-# 		end
-	end
-
-	def parse manifest_file
-=begin
-		#Set all the neccessary variables for this ODF
-		@encrypted_text #content.xml
-		
-		@hashing_algorithm
-		
-		@salt
-		@iteration_count
-		
-		@initialization_vector
-		@decryption_algorithm
-		
-		@checksum
-=end
+	def initialize decrypter_hash #Hash as in the ruby hashmap. Yeah might have to rename this
+		@hashing_algorithm = decrypter_hash['hashing_algorithm']
+		@salt = decrypter_hash['salt']
+		@iteration_count = decrypter_hash['iteration_count']
+		@initialization_vector = decrypter_hash['initialization_vector']
+		@decryption_algorithm = decrypter_hash['decryption_algorithm']		
+		@size = decrypter_hash['size']		
+		@checksum = decrypter_hash['checksum']
+		@key_size = decrypter_hash['key-size']
 	end
 	
 	# Dictionary :path to textfile of words separated by new line
@@ -42,8 +29,8 @@ class ODFDecrpter
 	end
 	
 	def check_password? password
-		hash = OpenSSL::Digest.digest(@hashing_algorithm, password.chomp!)
-		key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(hash, @salt, @iteration_count, 16)
+		hash = OpenSSL::Digest.digest(@hashing_algorithm, password)
+		key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(hash, @salt, @iteration_count, @key_size)
 		deflated_plain_text = decrypt key
 		if(inflate_plain_text(deflated_plain_text))
 			text = inflate_plain_text(deflated_plain_text)
@@ -61,6 +48,10 @@ class ODFDecrpter
 			return false
 		end
 	end
+	
+	attr_accessor:encrypted_text
+	
+	private
 	
 	def decrypt key
 		decipher = OpenSSL::Cipher.new(@decryption_algorithm)
@@ -85,32 +76,35 @@ class ODFDecrpter
 	
 	
 	#So global variables neccessary for decryption can be set by hand
-	attr_accessor:encrypted_text, :hashing_algorithm, :size, :salt, :iteration_count, :initialization_vector,:checksum, :decryption_algorithm
-	private :decrypt, :inflate_plain_text
+	#, :hashing_algorithm, :size, :salt, :iteration_count, :initialization_vector,:checksum, :decryption_algorithm
 end
 
 if __FILE__ == $0
-	decrypter = ODFDecrpter.new
-	# Setup Encrypted test
-	puts Dir.pwd
-	file = File.open('TestFiles/doc/content.xml','rb') #Content xml from odf
-	encrypted_text = ''
-	file.readlines.each do |line|
-	encrypted_text += line
-	end
+# 	# Setup Encrypted test
+# 	puts Dir.pwd
+		file = File.open(ARGV[1],'rb') #Content xml from odf
+		encrypted_text = ''
+		file.readlines.each do |line|
+			encrypted_text += line
+		end
+# 	decrypter.encrypted_text = encrypted_text
+# 	#Setup initvectors and salts
+# 	decrypter.initialization_vector = Base64.decode64("cVee3d2dh1M=")
+# 	decrypter.salt = Base64.decode64("lykzl3lw8LtNOx8WEL9gmQ==")
+# 	decrypter.hashing_algorithm = 'SHA1'
+# 	decrypter.iteration_count = 1024
+# 	decrypter.size = 31879840
+# 	decrypter.decryption_algorithm = 'BF-CFB'
+# 	decrypter.checksum = Base64.decode64('t2OrLDkrl/RUnmfRQpXimcWWH8o=')
+# 	#puts Base64.decode64('t2OrLDkrl/RUnmfRQpXimcWWH8o=')
+# 	#Really have to write that parser	
+	parser = ManifestParser.new ARGV[0]
+	puts parser.parse.inspect
+	decrypter = ODFDecrpter.new parser.decrypter_hash
 	decrypter.encrypted_text = encrypted_text
-	#Setup initvectors and salts
-	decrypter.initialization_vector = Base64.decode64("cVee3d2dh1M=")
-	decrypter.salt = Base64.decode64("lykzl3lw8LtNOx8WEL9gmQ==")
-	decrypter.hashing_algorithm = 'SHA1'
-	decrypter.iteration_count = 1024
-	decrypter.size = 31879840
-	decrypter.decryption_algorithm = 'BF-CFB'
-	decrypter.checksum = Base64.decode64('t2OrLDkrl/RUnmfRQpXimcWWH8o=')
-	#puts Base64.decode64('t2OrLDkrl/RUnmfRQpXimcWWH8o=')
-	#Really have to write that parser	
 	startTime = Time.new
-	puts decrypter.dictionary_attack ARGV[0] #Word List arg 1
+	decrypter.check_password? 'test'
+# 	puts decrypter.dictionary_attack ARGV[1] #Word List arg 1
 	endTime = Time.new
 	puts endTime - startTime
 end
