0bef984822bc3dc83258089440895d4b62034b03
ShaneQful <shane_quigley@hotmail.com>
2012-11-30 20:49:39 +0000

    Added license headers to the files

--- tc_odf_decrypter.rb
+++ tc_odf_decrypter.rb
@@ -1,3 +1,10 @@
+#!/usr/bin/ruby
+
+# Copyright (c) 2012 Shane Quigley
+# 
+# This software is MIT licensed see link for details
+# http://www.opensource.org/licenses/MIT
+
 require 'test/unit'
 require 'odf_decrypter.rb'
 
--- ts_all_tests.rb
+++ ts_all_tests.rb
@@ -1,3 +1,10 @@
+#!/usr/bin/ruby
+
+# Copyright (c) 2012 Shane Quigley
+# 
+# This software is MIT licensed see link for details
+# http://www.opensource.org/licenses/MIT
+
 require 'test/unit'
 require 'tc_manifest_parser.rb'
 require 'tc_odf_decrypter.rb'
